769f792a212261318615313a84e31b055cc02577
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-02 13:47:02 +0000

    now you are in bayes conspiracygit add .

--- app.py
+++ app.py
@@ -1,21 +1,83 @@
-from flask import Flask, render_template
-from form import SendForm
+from flask import Flask, render_template, flash, redirect, request
+from form import SendForm, LoginForm, UploadForm
 from config import Config
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
+import json
+import os
+from flask_bootstrap import Bootstrap
 
-app=Flask(__name__)
+app=Flask(__name__,static_folder="static_dir")
 app.config.from_object(Config)
-
+bootstrap = Bootstrap(app)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@app.route("/index")
+def index():
+    return render_template('index.html', title='Home')
 
 
-@app.route('/send')
+@app.route('/send',methods=['GET','POST'])
 def send():
     form = SendForm()
+    if form.validate_on_submit():
+
+        flash('Thank you for send data, {}'.format(form.username.data))
+    
+        #<shop>
+        name=form.username.data
+        company=form.company.data
+        url=form.url.data
+
+        data = {
+                "name":name,
+                "company":company,
+                "url":url
+                }
+
+        with open('user_info.json', 'w') as f:
+             f.write(json.dumps(data))
+        
+
+        #offer
+        f = form.upload.data
+        filename = str(name)+'.xls'
+        f.save(os.path.join('uploads/', filename))
+
+
+        return redirect('/yml')
+    
     return render_template('forms.html',title='Send', form=form)
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        name=form.username.data
+        password=form.password.data
+        data = {
+                "name":name,
+                "password":password
+        }
+        with open('user_info.json', 'w') as f:
+             f.write(json.dumps(data))
+        return redirect('/upload')
+    return render_template('login.html', title='Sign in', form=form)
+
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    form=UploadForm()
+    if form.validate_on_submit():
+        f = form.upload.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join('uploads/', filename))
+        
+        return redirect('/yml')
+    return render_template('upload.html', tiile='Upload File', form = form)
+
+@app.route('/yml', methods=['GET'])
+def yml():
+    return render_template('23.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- form.py
+++ form.py
@@ -1,13 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired
 
 class SendForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    company = StringField('Company', validators=[DataRequired()])
+    username = StringField('Короткое название магазина', validators=[DataRequired()])
+    company = StringField('Полное наименование компании', validators=[DataRequired()])
     url = StringField('Url')
-    platform = StringField('Platform')
-    version = StringField('Version')
-    agency = StringField('Agency')
-    email = StringField('Email')
+    upload = FileField(validators=[FileRequired(), FileAllowed(['xls', 'xlsx'], 'Excel only!'),])
+    submit = SubmitField('Send')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign in')
+
+class UploadForm(FlaskForm):
+    upload = FileField(validators=[FileRequired(), FileAllowed(['xls', 'xlsx'], 'Excel only!'),])
     submit = SubmitField('Send')
